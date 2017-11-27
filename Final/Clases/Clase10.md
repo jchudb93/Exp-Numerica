55270c0d2cdb14fc98f93455c61808156b5f0251
José Chu <jchudb93@gmail.com>
2017-11-27 15:55:11 -0500

    + clase 10

---
+++ Final/Clases/Clase10.md
@@ -0,0 +1,34 @@
+# Clase 10
+
+## Diseño de experimentos de 1 factor - parte 1
+
+### Etapas de la Metodología  para DOE
+
+  1. Definir los objetivos del experimentos
+  2. Identificar todas las posibles fuentes de variación, incluyendo:
+    - factores, tratamiento y sus niveles
+    - unidades experimentales
+    - factores nuisance (molestos), factores bloque, factores ruido y covariables
+  3. Elegir una regla de asignación de las unidades experimentales a los tratamientos.
+  4. Especificar las meddas con que se trabajará (la respuesta), el procedimiento experimental y anticiparse a las posibles dificultades.
+  5. Ejecutar un experimento piloto.
+  6. Especificar un modelo
+  7. Esquematizar los pasos del análisis: Estimaciones, contrastes, intervalos de confianza, diagnósticos y  bondad de ajuste del modelos.
+  8. Determinar el tamaño de la muestra.
+  9. Revisar las decisiones anteriores. Modificarlas en caso necesario.
+
+### ¿Qué es un diseño factorial?
+
+- Un **experimento factorial completo** es un experimiento que consta de 1 o más factores.
+- Por ejemplo, con dos factores y dos niveles de en cada factor, un experimiento factorial tendría en total cuatro combinaciones de tratamiento y se le denomina diseño factorial 2x2.
+
+### Modelo del diseño de experimientos de 1 factor
+
+- Los resultados se pueden modelar como una funcion lineal:
+$$
+y_{ij} = \mu + \tau_i + \epsilon_{i,j}
+$$
+- Donde:
+    * $\mu$ media total, el cual es un parámetro común a todos los tratamientos
+    * $\tau_i$ es el efecto del i-ésimo tratamiento del factor A sobre Y, y
+    * $\epsilon_{ij}$ es el error aleatorio con distribución $N(0,\sigma^2)$ y son independientes entre sí.
---
+++ Final/Clases/codigo_clase11_1dietas.r
@@ -0,0 +1,83 @@
+A <- c(62,60,63,59)
+B <- c(63,67,71,64,65,66)
+C <- c(68,66,71,67,68,68)
+D <- c(56,62,60,61,63,64,63,59)
+
+resultado <- c(A,B,C,D)
+dietas <- c(rep("A",length(A)),
+            rep("B",length(B)),
+            rep("C",length(C)),
+            rep("D",length(D)))
+
+misdatos <- data.frame(dietas,resultado)
+
+boxplot(resultado~dietas,misdatos, main="Tiempo de coagulación de la sangre segun dieta", ylab="Tiempo (segundos)")
+
+# ANOVA
+anova1 <- aov(resultado~dietas,misdatos)
+summary(anova1)
+
+# global Effect and Treatment Effect
+mu.est <- mean(misdatos$resultado)
+mean_treatment <-  aggregate(resultado~dietas,misdatos,mean)
+effect_treatment <-  aggregate(resultado~dietas,misdatos,function(x){mean(x)-mu.est})
+mu.est
+effect_treatment
+
+# Treatment Effect with a reference Level
+round(coefficients(anova1),2)
+
+# Changing Reference Level
+misdatos$dietas <- relevel(misdatos$dietas,ref="B")
+anova1 <- aov(resultado~dietas,misdatos)
+summary(anova1)
+round(coefficients(anova1),2)
+
+#================================================
+# DBCA
+#================================================
+ res <- c(13,16,5,22,24,4,18,17,1,39,44,22)
+ factor <- c(1,2,3,1,2,3,1,2,3,1,2,3)
+ bloque <- c(1,1,1,2,2,2,3,3,3,4,4,4)
+
+ length(res)
+ length(factor)
+ length(bloque)
+ 
+ boxplot(res~factor,midata, main="Retardo en el crecimiento de bacterias", 
+         ylab="Tiempo (segundos)",
+         xlab="Factor: Soluciones de lavado")
+ boxplot(res~bloque,midata, main="Retardo en el crecimiento de bacterias", 
+         ylab="Tiempo (segundos)",
+         xlab="Bloques: Días")
+ 
+ # Interacciones
+ interaction.plot(midata$factor,midata$bloque,midata$res,
+                  xlab="Factor: Soluciones de lavado",
+                  ylab="Tiempo (segundos)",
+                  leg.legend="Bloque")
+ 
+ #Opt1
+ midata <- data.frame(factor,bloque,res)
+ anova1 <- aov(res~as.factor(factor)+as.factor(bloque),midata)
+ summary(anova1)
+ 
+ #Opt2
+ midata <- data.frame(factor=as.factor(factor),bloque=as.factor(bloque),res)
+ anova1 <- aov(res~factor+bloque,midata)
+ summary(anova1)
+ 
+ # global Effect and Treatment Effect
+ mu.est <- mean(midata$res)
+ mean_treatment <-  aggregate(res~factor,midata,mean)
+ effect_treatment <-  aggregate(res~factor,midata,function(x){mean(x)-mu.est})
+ effect_block <-  aggregate(res~bloque,midata,function(x){mean(x)-mu.est})
+ mu.est
+ effect_treatment
+ effect_block
+ 
+ #Effects with control treatment
+ round(coefficients(anova1),4)
+ 
+ 
+ 
